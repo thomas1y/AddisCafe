40fea5c4f0f543e2b48ba3373e5ec04361b8fc13
Thomas Yohannes <thomasyohannes15@gmail.com>
2025-07-12 14:47:02 -0700

    Update constant.js

--- frontend/constant.js
+++ frontend/constant.js
@@ -1,7 +1,7 @@
 // constants.js
 
-// For now, just use local addresses
-export const NODE_ENV = 'development';
 
-export const BASE_API = "https://addiscafe-backend.onrender.com";
-export const STRIPE_REDIRECT_URL = 'http://localhost:5173';
+export const NODE_ENV = '';
+
+export const BASE_API = NODE_ENV === 'development' ? "http://localhost:4444" : "https://addiscafe-backend.onrender.com";
+export const STRIPE_REDIRECT_URL = NODE_ENV === 'development' ? "http://localhost:5173" : "https://addiscafe-vkvw.onrender.com/";
