1c7976a64dd596b97bbbeddadeaf283ec72f224e
thomas1y <thomasyohannes15@gmail.com>
2025-07-13 12:00:13 -0700

    Switch to HashRouter to fix routing on Render

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AdminContextProvider from "./context/AdminContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AdminContextProvider>
       <App />
     </AdminContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
