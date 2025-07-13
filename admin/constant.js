7142e570b1d1a0858938733268e370f0c73d65c1
thomas1y <thomasyohannes15@gmail.com>
2025-07-13 11:06:21 -0700

    final commit

--- admin/constant.js
+++ admin/constant.js
@@ -1,4 +1,4 @@
 let product = ''
 
 // constants.js (copy this into your project)
-export const BASE_API_URL = "https://addiscafe-backend.onrender.com";
+export const BASE_API_URL = product === "development" ? "http://localhost:4444" : "https://addiscafe-backend.onrender.com";
--- frontend/constant.js
+++ frontend/constant.js
@@ -4,8 +4,6 @@
 export const NODE_ENV = '';
 
 export const BASE_API = NODE_ENV === 'development' ? "http://localhost:4444" : "https://addiscafe-backend.onrender.com";
-<<<<<<< HEAD
+
 export const STRIPE_REDIRECT_URL = NODE_ENV === 'development' ? "http://localhost:5173" : "https://addiscafe-vkvw.onrender.com/";
-=======
-export const STRIPE_REDIRECT_URL = NODE_ENV === 'development' ? "http://localhost:5173" : "https://addiscafe-vkvw.onrender.com/";
->>>>>>> 40fea5c4f0f543e2b48ba3373e5ec04361b8fc13
+
