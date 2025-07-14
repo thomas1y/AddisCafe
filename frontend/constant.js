9381ed666f45c13f9a8b495cbef393903d9bec54
thomas1y <thomasyohannes15@gmail.com>
2025-07-14 15:52:41 -0700

    update constant js

--- frontend/constant.js
+++ frontend/constant.js
@@ -5,5 +5,5 @@ export const NODE_ENV = '';
 
 export const BASE_API = NODE_ENV === 'development' ? "http://localhost:4444" : "https://addiscafe-backend.onrender.com";
 
-export const STRIPE_REDIRECT_URL = NODE_ENV === 'development' ? "http://localhost:5173" : "https://addiscafe-vkvw.onrender.com/";
+export const STRIPE_REDIRECT_URL = NODE_ENV === 'development' ? "http://localhost:5173" : "https://addiscafe-rp7w.onrender.com";
 
